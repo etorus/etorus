74a3c6c13b9de433dc463e4a5f8cb08e9742f3d7
viniciuspalma <pockvini@gmail.com>
2018-05-05 01:46:41 -0300

    install react-native-audio

--- android/app/src/main/java/com/etorus/MainApplication.java
+++ android/app/src/main/java/com/etorus/MainApplication.java
@@ -3,6 +3,7 @@ package com.etorus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNSoundPackage(),
           new LinearGradientPackage(),
           new FBSDKPackage(mCallbackManager)
       );
