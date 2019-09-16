f3bdc7e9ff2bff01921c0b2ba9c44adeabf9c4dd
Laniakea <grwgithub@gmail.com>
2019-09-16 16:02:58 -0300

    ( upgrade ) -> Android Files
    Update the Android files according to React Native diff purge
    
    The changes files are on the link below:
    https://github.com/react-native-community/rn-diff-purge/compare/release/0.54.3..release/0.60.5

--- android/app/src/main/java/com/etorus/MainApplication.java
+++ android/app/src/main/java/com/etorus/MainApplication.java
@@ -1,7 +1,15 @@
 package com.etorus;
 
 import android.app.Application;
+import android.util.Log;
 
+
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -11,15 +19,11 @@ import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.facebook.CallbackManager;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -29,15 +33,21 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNDeviceInfo(),
-          new PickerPackage(),
-          new ReactNativePushNotificationPackage(),
-          new RNSoundPackage(),
-          new LinearGradientPackage(),
-          new FBSDKPackage(mCallbackManager)
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
+      // return Arrays.<ReactPackage>asList(
+      //     new MainReactPackage(),
+      //       new RNDeviceInfo(),
+      //     new PickerPackage(),
+      //     new ReactNativePushNotificationPackage(),
+      //     new RNSoundPackage(),
+      //     new LinearGradientPackage(),
+      //     new FBSDKPackage(mCallbackManager)
+      // );
     }
 
     @Override
@@ -55,9 +65,5 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-  }
-
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
   }
 }
